4b639bb8b2c4e6a3678b899619d921e400303cdb
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2020-02-10 08:02:33 -0600

    Add files via upload

--- PKGBUILD
+++ PKGBUILD
@@ -7,15 +7,15 @@ pkgrel=1
 arch=('any')
 license=()
 makedepends=()
-depends_x86_64=('go' 'squashfs-tools' 'reprepro' 'arch-install-scripts')
-depends_aarch64=('go' 'reprepro' 'arch-install-scripts' 'wget')
+depends_x86_64=('squashfs-tools')
+depends=('go' 'reprepro' 'arch-install-scripts' 'wget')
 source=(
 "chrootstrap.sh"
 "chrootstrap-alarm.sh"
 "create-deb-pkg.sh"
 "create-deb-repo.sh"
 #"pacman.conf"
-'mirrorlist-amd64'
+#'mirrorlist-amd64'
 #'mirrorlist-arm'
 "PKGBUILD.chrootstrap"
 "skyupdate.sh"
@@ -24,7 +24,6 @@ sha256sums=('43b9667be5959c6f2567d8bdcfee83bbe9742ec7ab8599ff438427eff40a7c20'
             '8c20b86660ec76f08f20e95f50f93f6d0f6c5b3f5f04eae950a682edcd1db474'
             '230e3077036b731f6ca9c3bb11000158d8d3368ef4666281f0f5431cc7fceba0'
             '719b37c4bb4b64fdca76376484d4fa20d5fccf4e92d193f9835039055b18f7c0'
-            '8b5e78aa55c41039038b4da050219192357e6e524b78bdf80a87cc65e0bab362'
             '43be60c79c87745707cfdfa2cdeb8456747877a920674f4a249f00b2b2306988'
             'c0aef2ae58e25be224eaabea53b353fd6e336932806ec83529a5429c23ad3b55')
 systemarchitecture=$( uname -m )
@@ -57,6 +56,6 @@ fi
 	chmod +x ${pkgdir}/usr/bin/*
   install -Dm755 ${srcdir}/*.pkg.tar.xz ${pkgdir}/usr/lib/skycoin/${pkgname1}/
   #install -Dm755 ${srcdir}/pacman.conf ${pkgdir}/usr/lib/skycoin/${pkgname1}/
-  install -Dm755 ${srcdir}/mirrorlist-amd64 ${pkgdir}/usr/lib/skycoin/${pkgname1}/
+  #install -Dm755 ${srcdir}/mirrorlist-amd64 ${pkgdir}/usr/lib/skycoin/${pkgname1}/
   #install -Dm755 ${srcdir}/mirrorlist-arm ${pkgdir}/usr/lib/skycoin/${pkgname1}/
 }
--- skyupdate.sh
+++ skyupdate.sh
@@ -8,11 +8,17 @@
 #local debian package repo hosted on LAN
 #/root/skyupdate/squashfs-root/deb
 set -x
+#require root
   if [[ $EUID -ne 0 ]]; then
      echo "You must be root to do this." 1>&2
      exit 100
   fi
+#This gets unset if non-debian distro is detected
 installpackage="yes"
+#default chroot dir
+chrootdir=squashfs-root
+
+########### CHECK IF THIS SCRIPT IS RUNNING ON A DEBIAN HOST #################
 DEBIANCHECK=$(cat /etc/os-release | grep Debian)
 ARMBIANCHECK=$(cat /etc/os-release | grep Armbian)
 RASPBIANCHECK=$(cat /etc/os-release | grep Raspbian)
@@ -23,30 +29,62 @@ echo "YOUR SYSTEM:"
 cat /etc/os-release
 installpackage="no"
 #exit
+fi
+
+#### TO UPDATE THE PACKAGE CONTAINING THIS SCRIPT FOR ARM ARCHITECTURES #####
+#  `skyupdate skyupdate arm64`
+# to update the skyupdate debian package on the host architecture;
+#from archlinux run ``./buildskyupdater.sh deb`
+#https://github.com/skyfleet/sky-update
+
+#require architecture argument for skyupdate
 if [ "$1" ==  "skyupdate" ] && [ -z $2 ]; then
   echo "please specify an architecture"
+  #list of available architectures here
   exit
 fi
-chrootdir=squashfs-root
 
+#only allow skyupdate to update itself on foreign architectures
+if [ "$1" !=  "skyupdate" ] && [ ! -z $2 ]; then
+  echo "this feature is currently not suppoted"
+  exit
+fi
+
+#set systemarch as the second argument provided to this script
 if [ "$1" ==  "skyupdate" ] && [ ! -z $2 ]; then
 SYSTEMARCH=$2
+SYSTEMARCHTEST=$(dpkg --print-architecture)
+#prevent specifying the host architecture
+if [ "$SYSTEMARCH" ==  "$SYSTEMARCHTEST" ]; then
+echo "Error: host architecture specified."
+exit
+else
+#use this for determining chroot method
+foreignchroot=yes
+fi
+#append architecture to chrootdir
 chrootdir=$chrootdir-$SYSTEMARCH
 else
+#Default ; ask dpkg for architecture
 SYSTEMARCH=$(dpkg --print-architecture)
+#default path
 mkdir -p /root/skyupdate && cd /root/skyupdate
 fi
-#don't do the package cache sharing on foreign architectures
-if [ -z $2 ] && [ ! -z $ARCHCHECK ]; then
-  #testing considerations ; configure host with read only cache
-  sudo pacman -Syy
+#package cache sharing - prevent on foreign architectures
+if [ "$foreignchroot" != "yes" ] && [ ! -z $ARCHCHECK ]; then
+#switch for later in the script
+availablehostpackagecache=yes
+#sync pacman
+sudo pacman -Syy
+#set up shared cache
 ln -sf /var/lib/pacman/sync/*.db /var/cache/pacman/pkg
  cd /var/cache/pacman/pkg
  killall darkhttpd
 nohup darkhttpd . > /dev/null 2>&1 &
-fi
+cd /root/skyupdate
 fi
 
+################ ARCHLINUXARM ROOT FILESYSTEM LINKS #######################
 ALARMBASEURL="http://os.archlinuxarm.org/os/"
 ARMV5GZ="ArchLinuxARM-armv5-latest.tar.gz"
 ARMV6GZ="ArchLinuxARM-armv6-latest.tar.gz"
@@ -55,10 +93,12 @@ ARMV8GZ="ArchLinuxARM-armv8-latest.tar.gz"
 AARCH64GZ="ArchLinuxARM-aarch64-latest.tar.gz"
 
 
+############### DOWNLOAD AND EXTRACT ROOT FILESYSTEM #####################
 #SYSTEMARCH uses the debian naming convention for architectures
 if [[ $SYSTEMARCH == "amd64" ]]; then
   if [ ! -d $chrootdir ]; then
-    mkdir $chrootdir
+    #unsquashfs makes the chrootdir
+    #mkdir $chrootdir
 #obtain the latest rootFS sources
 MAGNETLINK=$(curl -s https://www.archlinux.org/download/ | sed -rn '/magnet/{s/.*(magnet:[^"]*).*/\1/g;p}')
 MAGNETLINK="${MAGNETLINK%%&*}"
@@ -129,34 +169,84 @@ if [[ $SYSTEMARCH == "armv8" ]]; then
     bsdtar -xpf $ARMV8GZ -C $chrootdir
   fi
 fi
-
+#error on invalid architecture
 if [ -z $chrootdir ]; then
   echo "Invalid or unsupported architecture"
   exit
 fi
 
-cp -b /etc/resolv.conf $chrootdir/etc/resolv.conf
+##################### CHROOTING SECTION #################################
+
+#default chrooting
+if [ "$foreignchroot" != "yes" ]; then
+#The package is assumed to be installed if it already exists there
 if [ ! -f $chrootdir/root/*.pkg.tar.xz ]; then
 cp -b /usr/lib/skycoin/skyupdate/*.pkg.tar.xz $chrootdir/root/
-arch-chroot $chrootdir /bin/bash -c "pacman -U /root/*.pkg.tar.xz --noconfirm"
+cd $chrootdir/root/
+pkgstoinstall=$(ls *.pkg.tar.xz)
+cd ..
+cd ..
+arch-chroot $chrootdir pacman -U /root/$pkgstoinstall --noconfirm --noconfirm
+echo
 fi
-if [ -z $2 ] && [ ! -z $ARCHCHECK ]; then
-#testing consierations - update the chroot from the host system package cache
-arch-chroot $chrootdir /bin/bash -c "[ ! -f /etc/pacman.d/mirrorlist.bak ] && cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak && { echo -e 'Server = http://127.0.0.1:80'; cat /etc/pacman.d/mirrorlist.bak; } > /etc/pacman.d/mirrorlist"
+if [ "$availablehostpackagecache" == "yes" ]; then
+cp -b /usr/lib/skycoin/skyupdate/mirrorlist-$SYSTEMARCH $chrootdir/etc/pacman.d/mirrorlist
+#now we can update the chroot from the host system package cache
+#arch-chroot $chrootdir -c "[ ! -f /etc/pacman.d/mirrorlist.bak ] && cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak && { echo -e 'Server = http://127.0.0.1:80'; cat /etc/pacman.d/mirrorlist.bak; } > /etc/pacman.d/mirrorlist"
 #the above returns nothing so to keep the script from failing
 echo
 fi
-
-arch-chroot $chrootdir /bin/bash -c "chrootstrap $1"
-if [ -z $2 ] && [ "$installpackage" == "yes" ]; then
-dpkg --force-overwrite -i $chrootdir/deb/*.deb
+arch-chroot $chrootdir chrootstrap $1
+echo
 fi
+
+#THIS IS FAILING TO RESOLVE DNS
+#for this to work, `systemctl enable systemd-binfmt.service`
+#if [ "$foreignchroot" == "yes" ]; then
+#  cp -b  /usr/bin/qemu-aarch64-static $chrootdir/usr/bin
+  #The package is assumed to be installed if it already exists there
+#  if [ ! -f $chrootdir/root/*.pkg.tar.xz ]; then
+#  cp -b /usr/lib/skycoin/skyupdate/*.pkg.tar.xz $chrootdir/root/
+#  cp -b /usr/lib/skycoin/skyupdate/pacman.conf $chrootdir/etc/
+#  arch-chroot $chrootdir /bin/bash -c "pacman -U /root/*.pkg.tar.xz --noconfirm --noconfirm"
+#  echo
+#  fi
+#  arch-chroot $(pwd)/$chrootdir /bin/bash -c "chrootstrap $1"
+#  echo
+#  fi
+
+####################### END CHROOTING SECTION ############################
+
+#test for created package
+cd $chrootdir/deb
+createdpackage=$(ls $1*.deb)
+
+if [ -z $createdpackage ]]; then
+ls $chrootdir/deb/*.deb
+echo -e "There have been errors, no package was created."
+exit
+fi
+
+####################### PACKAGE INSTALLTION ###############################
+#only on debian
+if [ "$foreignchroot" != "yes" ] && [ "$installpackage" == "yes" ]; then
+  createdpackage=$(ls $chrootdir/deb/${1}*.deb)
+echo -e "install package $createdpackage?"
+dpkg --force-overwrite -i $createdpackage
+fi
+
+####################### REPOSITORY CREATION ###############################
+
+echo "packages:"
+ls $chrootdir/deb/${1}*.deb
 read -p "Create package repo to update nodes? (y/n)" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
+#removes an existing repo
   rm -rf $chrootdir/deb/conf
   rm -rf $chrootdir/deb/db
   rm -rf $chrootdir/deb/dists
   rm -rf $chrootdir/deb/pool
+#create a repo
   create-deb-repo $chrootdir/deb
 fi
